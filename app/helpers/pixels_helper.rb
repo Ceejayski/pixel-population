13314dd80648d1fece9514055cbe2994aeba8b4a
Ceejayski <sirchijioke1@gmail.com>
2021-07-16 11:12:55 -0700

    Setup Datatable

--- app/helpers/pixels_helper.rb
+++ app/helpers/pixels_helper.rb
@@ -6,7 +6,7 @@ module PixelsHelper
     elsif pixel.children.size == 1
       text = "#{pixel.name} has only #{pixel.children[0].name} as his child"
     elsif pixel.children.size == 2
-      text = "#{pixel.name} has <span>only</span> #{pixel.children[0].name} and #{pixel.children[1].name} as his children"
+      text = "#{pixel.name} has only #{pixel.children[0].name} and #{pixel.children[1].name} as his children"
     else
       text = "#{pixel.name} has  #{pixel.children[0].name}, #{pixel.children[1].name} and #{pixel.children.size - 2} more as his children"
     end
